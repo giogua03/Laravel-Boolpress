edad09e4501013e25f188b224f176881b4ad6e06
giogua03 <giorgiogua03@gmail.com>
2023-02-07 18:04:25 +0100

    risolto problema immagini

---
+++ resources/js/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <NavBar />
+        <main>
+            <div class="container">
+                <router-view></router-view>
+            </div>
+        </main>
+        <FooterComponent />
+    </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar';
+import FooterComponent from './components/FooterComponent';
+
+export default {
+  name: 'App',
+  components: {
+    NavBar, // NavBar: NavBar,
+    FooterComponent, // FooterComponent: FooterComponent,
+  },
+}
+</script>
+
+<style lang="scss">
+    @import '~bootstrap/scss/bootstrap';
+</style>
